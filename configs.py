b70c6a87d7428728a32e282470ab9bb4b30f60dc
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2023-04-15 04:13:45 +0330

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 14699743))
+    API_ID = (os.environ.get("API_ID", 14699743)
     API_HASH = os.environ.get("API_HASH", "0cef89ed2c8025c16d2b4d42a1b8d792" )
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5115356918:AAFH3T-1f2x4ZdikRQnNoOXXgonLUlwryAQ")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
