2df276523d248670c9108a683c6579fc4de98c4e
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2023-05-14 08:38:01 +0330

    Add files via upload

--- configs.py
+++ configs.py
@@ -6,17 +6,17 @@ class Config(object):
     API_ID = (os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
+    SESSION_NAME = os.environ.get("SESSION_NAME")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
-    DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
-    TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 4))
+    DOWN_PATH = os.environ.get("DOWN_PATH")
+    TIME_GAP = int(os.environ.get("TIME_GAP"))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS"))
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
     MONGODB_URI = os.environ.get("MONGODB_URI")
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 5059280908))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 
     START_TEXT = """
 𝐇𝐢 𝐃𝐞𝐚𝐫 𝐔𝐬𝐞𝐫 , 𝐈 𝐚𝐦 𝐕𝐢𝐝𝐞𝐨 𝐌𝐞𝐫𝐠𝐞 𝐁𝐨𝐭!
